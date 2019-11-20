2c52410e77b80d8906ccf7e304f7030d32f621a5
DarthChurros <tnarayan00@stuy.edu>
2019-11-19 19:11:53 -0500

    made makefile, wrote includes

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+int main(int argc, char* argv[]) {
+
+  return 0;
+}
